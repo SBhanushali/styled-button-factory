40a8078ee4e9bdab6a77b16bc2d0d946b4e32403
SBhanushali <shivam.n.bhanushali@gmail.com>
2020-12-22 21:52:53 +0530

    Make shadow prop to  show box shadow

--- src/app/components/Layout/Box.js
+++ src/app/components/Layout/Box.js
@@ -1,9 +1,13 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { space, color, border, layout, flexbox, grid } from "styled-system";
 
+const shadow = css`
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+`;
+
 const Box = styled.div`
-  box-shadow: ${(props) =>
-    props.shadow ? `0px 2px 10px rgba(0, 0, 0, 0.1);` : ""};
+  ${(props) => props.shadow && shadow};
   ${space}
   ${color}
   ${border}
