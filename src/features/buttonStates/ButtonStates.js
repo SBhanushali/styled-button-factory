d8df6a1046b27b3649c114978f1c788d4b4b9c01
SBhanushali <shivam.n.bhanushali@gmail.com>
2020-12-24 12:13:36 +0530

    Remove  max-content width from button states

--- src/features/buttonStates/ButtonStates.js
+++ src/features/buttonStates/ButtonStates.js
@@ -3,7 +3,7 @@ import { Box, Text, RadioButtonGroup } from "../../app/components";
 const values = ["Default", "Hover", "Active", "Disabled"];
 const ButtonStates = () => {
   return (
-    <Box shadow width="max-content" borderRadius="10px" p="10px">
+    <Box shadow borderRadius="10px" p="10px" pb="15px">
       <Box>
         <Text fontWeight="bold" fontSize="14px" mb="2em">
           Button States
