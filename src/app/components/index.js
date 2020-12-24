8c06ba341ef08add71662e75e706689e21a7eee7
SBhanushali <shivam.n.bhanushali@gmail.com>
2020-12-24 12:10:57 +0530

    Upadate component export

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -2,4 +2,7 @@ export { default as Navbar } from "./Navbar/Navbar";
 export { default as Box } from "./Layout/Box";
 export { default as Row } from "./Layout/Row";
 export { default as Col } from "./Layout/Column";
+export { default as IconContainer } from "./Layout/IconContainer";
 export { default as RadioButtonGroup } from "./RadioButtonGroup/RadioGroupButton";
+export { default as Text } from "./Typography/Text";
+export { default as ColorPicker } from "./ColorPicker";
