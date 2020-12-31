2e541bed1b61e4c45c8e0b52865e3e38fe47d5d7
SBhanushali <shivam.n.bhanushali@gmail.com>
2020-12-31 12:16:17 +0530

    Create useDebouncedEffect hook to skip frequent renders

---
+++ src/app/hooks/useDebouncedEffect.js
@@ -0,0 +1,15 @@
+import { useEffect, useCallback } from "react";
+
+export const useDebouncedEffect = (effect, delay, deps) => {
+  const callback = useCallback(effect, deps);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      callback();
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [callback, delay]);
+};
