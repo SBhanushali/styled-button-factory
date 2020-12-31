43cf954d51cd1ef6022379af23f851d22d1c23bf
SBhanushali <shivam.n.bhanushali@gmail.com>
2021-01-01 01:46:29 +0530

    update switch logic

---
+++ src/features/ButtonBorderSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const buttonBorderSlice = createSlice({
+  name: "buttonBorder",
+  initialState: {
+    hasBorder: true,
+    borderType: undefined,
+    borderColor: "#ffffff",
+    borderWidth: 0,
+    borderRadius: 0,
+  },
+  reducers: {
+    setHasBorder: (state, action) => {
+      state.hasBorder = !state.hasBorder;
+    },
+    setBorderType: (state, action) => {
+      state.borderType = action.payload;
+    },
+    setBorderColor: (state, action) => {
+      state.borderColor = action.payload;
+    },
+    setBorderWidth: (state, action) => {
+      state.borderWidth = action.payload;
+    },
+    setBorderRadius: (state, action) => {
+      state.borderRadius = action.payload;
+    },
+  },
+});
+
+export const {
+  setHasBorder,
+  setBorderType,
+  setBorderColor,
+  setBorderWidth,
+  setBorderRadius,
+} = buttonBorderSlice.actions;
+
+export default buttonBorderSlice.reducer;
