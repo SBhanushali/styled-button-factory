318e4457f1f8eafa287cb06abc0d67eded7ffe29
SBhanushali <shivam.n.bhanushali@gmail.com>
2021-01-01 01:39:50 +0530

    Connect buttonBorder to store

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import canvasBackgroundReducer from "../features/canvasBackgroundSlice";
 import buttonBackgroundReducer from "../features/ButtonBackgroundSlice";
+import buttonBorderReducer from "../features/ButtonBorderSlice";
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
     canvas: canvasBackgroundReducer,
     buttonBackground: buttonBackgroundReducer,
+    buttonBorder: buttonBorderReducer,
   },
 });
