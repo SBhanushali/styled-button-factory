5e7679adbe4e27a19eb081d95aa3b5aa5014d319
SBhanushali <shivam.n.bhanushali@gmail.com>
2021-01-01 21:56:34 +0530

    Button shadow slice for hover

---
+++ src/features/HoverButtonShadowSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const hoverButtonShadowSlice = createSlice({
+  name: "buttonShadow",
+  initialState: {
+    hasShadow: true,
+    shadowType: "Outset",
+    shadowColor: "#ffffff",
+    horizontalPlacement: 0,
+    verticalPlacement: 0,
+    blur: 0,
+    spread: 0,
+  },
+  reducers: {
+    setHasShadow: (state, action) => {
+      state.hasShadow = !state.hasShadow;
+    },
+    setShadowColor: (state, action) => {
+      state.shadowColor = action.payload;
+    },
+    setShadowType: (state, action) => {
+      state.shadowType = action.payload;
+    },
+    setHorizontalPlacement: (state, action) => {
+      state.horizontalPlacement = action.payload;
+    },
+    setVerticalPlacement: (state, action) => {
+      state.verticalPlacement = action.payload;
+    },
+    setBlur: (state, action) => {
+      state.blur = action.payload;
+    },
+    setSpread: (state, action) => {
+      state.spread = action.payload;
+    },
+  },
+});
+
+export const {
+  setHasShadow,
+  setShadowType,
+  setShadowColor,
+  setHorizontalPlacement,
+  setVerticalPlacement,
+  setBlur,
+  setSpread,
+} = hoverButtonShadowSlice.actions;
+
+export default hoverButtonShadowSlice.reducer;
