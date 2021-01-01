8efed8e71358e3cd584cd7ce6eae4a396ca0c6fa
SBhanushali <shivam.n.bhanushali@gmail.com>
2021-01-01 22:02:46 +0530

    Button default styles for hover

---
+++ src/features/HoverButtonDefaultSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const hoverButtonDefaultSlice = createSlice({
+  name: "buttonDefault",
+  initialState: {
+    fontSize: 0,
+    textColor: "#000000",
+  },
+  reducers: {
+    setFontSize: (state, action) => {
+      state.fontSize = action.payload;
+    },
+    setTextColor: (state, action) => {
+      state.textColor = action.payload;
+    },
+  },
+});
+
+export const { setFontSize, setTextColor } = hoverButtonDefaultSlice.actions;
+
+export default hoverButtonDefaultSlice.reducer;
