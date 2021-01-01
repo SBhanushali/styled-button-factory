640d755801b5f125c60046786a8b89c22a99e829
SBhanushali <shivam.n.bhanushali@gmail.com>
2021-01-01 22:04:10 +0530

    Add button default reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import canvasBackgroundReducer from "../features/canvasBackgroundSlice";
 import buttonBackgroundReducer from "../features/ButtonBackgroundSlice";
 import buttonBorderReducer from "../features/ButtonBorderSlice";
 import buttonShadowReducer from "../features/ButtonShadowSlice";
+import buttonDefaultReducer from "../features/ButtonDefaultSlice";
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     canvas: canvasBackgroundReducer,
+    buttonDefault: buttonDefaultReducer,
     buttonBackground: buttonBackgroundReducer,
     buttonBorder: buttonBorderReducer,
     buttonShadow: buttonShadowReducer,
