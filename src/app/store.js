7906c5feb4498c10b49200146cb969af430ac1a7
SBhanushali <shivam.n.bhanushali@gmail.com>
2021-01-01 14:33:14 +0530

    Connect buttonShadowReducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import counterReducer from "../features/counter/counterSlice";
 import canvasBackgroundReducer from "../features/canvasBackgroundSlice";
 import buttonBackgroundReducer from "../features/ButtonBackgroundSlice";
 import buttonBorderReducer from "../features/ButtonBorderSlice";
+import buttonShadowReducer from "../features/ButtonShadowSlice";
 
 export default configureStore({
   reducer: {
@@ -10,5 +11,6 @@ export default configureStore({
     canvas: canvasBackgroundReducer,
     buttonBackground: buttonBackgroundReducer,
     buttonBorder: buttonBorderReducer,
+    buttonShadow: buttonShadowReducer,
   },
 });
